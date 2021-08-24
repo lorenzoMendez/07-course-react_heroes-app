586106d1fade54660155820d14b8531a418221e3
lorenzo <lor90td@gmail.com>
2021-08-24 13:52:20 -0500

    changes

---
+++ src/components/heroes/HeroesScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroesScreen = () => {
+    return (
+        <div>
+            <h1>Heroes Screen</h1>
+        </div>
+    )
+}
+
+
+export default HeroesScreen;
